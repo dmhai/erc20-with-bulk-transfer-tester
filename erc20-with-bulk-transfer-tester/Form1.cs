cbb58d496142114160bc7c7a72319f26432cb76c
Kevin Wong <wertykevin91@gmail.com>
2018-09-24 15:17:32 +0800

    Add project files.

---
+++ erc20-with-bulk-transfer-tester/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace erc20_with_bulk_transfer_tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
